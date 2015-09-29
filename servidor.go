74ea9b547d32fbd6b06d12c23a90e753f87a9ed5
Rodrigo Martins <rodrigo@rrmartins.com>
2015-09-29 11:28:19 -0300

    change validation

--- servidor.go
+++ servidor.go
@@ -11,11 +11,7 @@ func main() {
 	http.HandleFunc("/tempo", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%s", time.Now().Format("2006-01-02 15:04:02"))
 	})
-	var port string
-	if os.Getenv("PORT") != "" {
-		port = os.Getenv("PORT")
-	} else {
-		port = "8080"
-	}
+	port := map[bool]string{true: os.Getenv("PORT"), false: "8080"}[ os.Getenv("PORT") != ""]
+
 	http.ListenAndServe(fmt.Sprintf(":%s", port),nil)
 }
